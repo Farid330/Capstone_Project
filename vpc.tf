50df710e9c7e383c3c2474fa353689836d7b1878
Farid330 <orujov.farid94@gmail.com>
2024-11-27 16:09:51 +0100

    adding public route table

--- vpc.tf
+++ vpc.tf
@@ -60,3 +60,17 @@ resource "aws_internet_gateway" "igw" {
     Name = "IGW for Wordpress"
   }
 }
+
+#Route table
+#Public route table
+
+resource "aws_route_table" "public route table" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "public-route-table"
+  }
+}
