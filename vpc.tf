b6b1a350190005818532eca485081ce66623cabb
Farid330 <orujov.farid94@gmail.com>
2024-11-27 16:38:07 +0100

    correcting subnet_id also to public-1

--- vpc.tf
+++ vpc.tf
@@ -75,7 +75,7 @@ resource "aws_route_table" "public_route_table" {
 
 #Associate Public Subnets with the Public Route Table
 resource "aws_route_table_association" "public_1_assoc" {
-  subnet_id      = aws_subnet.public_1.id
+  subnet_id      = aws_subnet.public-1.id
   route_table_id = aws_route_table.public_route_table.id
   depends_on     = [aws_route_table.public_route_table, aws_subnet.public-1]
 }
