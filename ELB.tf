b9f3f316de123ef0651db5af8fafff446334403d
Farid330 <orujov.farid94@gmail.com>
2024-11-27 17:29:40 +0100

    adding ELB security group before ELB

--- ELB.tf
+++ ELB.tf
@@ -1,14 +1,22 @@
-# Application Load Balancer (ALB)
-resource "aws_lb" "wordpress_lb" {
-  name               = "wordpress-alb"
-  internal           = false  
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.lb_sg.id]
-  subnets            = [aws_subnet.public-1.id, aws_subnet.public-2.id]
-  enable_deletion_protection = false
-  idle_timeout       = 60
+# Security Group for ALB
+resource "aws_security_group" "lb_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   tags = {
-    Name = "wordpress-alb"
+    Name = "wordpress-lb-sg"
   }
 }
