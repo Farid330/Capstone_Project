c4bcea08a67764921dc5a2ac4bbf1a0075dbd94e
Farid330 <orujov.farid94@gmail.com>
2024-11-27 14:54:47 +0100

    add 2nd public subnet

--- vpc.tf
+++ vpc.tf
@@ -21,4 +21,14 @@ resource "aws_subnet" "public_1" {
   }
 }
 
+resource "aws_subnet" "public-2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.3.0/24"
+  availability_zone = "eu-central-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-wordpress-2"
+  }
+}
 
