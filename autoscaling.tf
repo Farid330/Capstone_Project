90009da6bbdeb8cd05446ac5655951b14c1d7ab3
Farid330 <orujov.farid94@gmail.com>
2024-11-28 16:19:02 +0100

    removing autoscaling

--- autoscaling.tf
+++
@@ -1,9 +0,0 @@
-#Launch Template
-resource "aws_launch_template" "dev-launch-template" {
-  name = "WordPress_Ec2_LaunchTemplate"
-  image_id = data.aws_ami.amazon_linux.id
-  instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.wordpress_sg.id]
-  key_name = var.key_name
-
- }
