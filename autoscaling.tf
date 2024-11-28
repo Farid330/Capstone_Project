d43d4afa645d907255b11ef9721f41562c95fe31
Farid330 <orujov.farid94@gmail.com>
2024-11-28 16:21:07 +0100

    adding autoscaling without userdata

---
+++ autoscaling.tf
@@ -0,0 +1,12 @@
+#Launch Template
+resource "aws_launch_template" "dev-launch-template" {
+  name = "WebserverLaunchTemplate"
+  //image_id = data.aws_ami.amzLinux.id
+  image_id = "ami-05c9d06873bde2328"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.sg_vpc.id]
+  key_name = "deham9-iam"
+  #user_data = file("userdatalaunchtemplate.sh")
+  #user_data = "${base64encode(data.template_file.userdatatemplate.rendered)}" 
+
+ }
