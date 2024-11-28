cc9558f62a8f3da56e0b20a0fe45c39704bb4adf
Farid330 <orujov.farid94@gmail.com>
2024-11-28 15:36:25 +0100

    correcting security group in launch template

--- autoscaling.tf
+++ autoscaling.tf
@@ -3,7 +3,7 @@ resource "aws_launch_template" "dev-launch-template" {
   name = "WordPress_Ec2_LaunchTemplate"
   image_id = data.aws_ami.amazon_linux.id
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.sg_vpc.id]
+  vpc_security_group_ids = [aws_security_group.wordpress_sg.id]
   key_name = var.key_name
   #user_data = file("userdatalaunchtemplate.sh")
   #user_data = "${base64encode(data.template_file.userdatatemplate.rendered)}" 
