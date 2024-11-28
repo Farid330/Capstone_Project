b7612973e765a7dbe8aeb767d20d2069b90553bd
Farid330 <orujov.farid94@gmail.com>
2024-11-28 15:34:22 +0100

    adding autoscaling without userdata for now

---
+++ autoscaling.tf
@@ -0,0 +1,11 @@
+#Launch Template
+resource "aws_launch_template" "dev-launch-template" {
+  name = "WordPress_Ec2_LaunchTemplate"
+  image_id = data.aws_ami.amazon_linux.id
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.sg_vpc.id]
+  key_name = var.key_name
+  #user_data = file("userdatalaunchtemplate.sh")
+  #user_data = "${base64encode(data.template_file.userdatatemplate.rendered)}" 
+
+ }
