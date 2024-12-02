02f98add7f7b4e2980f869bee9e6553314c18923
Farid330 <orujov.farid94@gmail.com>
2024-12-02 14:37:51 +0100

    adding sg for aurora db

--- rds.tf
+++ rds.tf
@@ -7,3 +7,28 @@ resource "aws_db_subnet_group" "aurora_subnet_group" {
     Name = "Aurora DB Subnet Group"
   }
 }
+
+#Adding security group for RDS Aurora
+resource "aws_security_group" "rds_sg" {
+  name        = "aurora-rds-sg"
+  description = "Security group for Aurora RDS cluster"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = var.cidr_blocks # e.g., CIDR blocks allowed to access the database
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "RDS Security Group"
+  }
+}
