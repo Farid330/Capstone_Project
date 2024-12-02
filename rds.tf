56c75b0a2d1a81dfad6780148ecacf7df278e6cb
Farid330 <orujov.farid94@gmail.com>
2024-12-02 15:07:17 +0100

    configuring auora instances

--- rds.tf
+++ rds.tf
@@ -43,9 +43,27 @@ resource "aws_rds_cluster" "aurora_cluster" {
   database_name           = var.db_name
   vpc_security_group_ids  = [aws_security_group.rds_sg.id]
   db_subnet_group_name    = aws_db_subnet_group.aurora_subnet_group.name
+  skip_final_snapshot       = true
+  final_snapshot_identifier = "aurora-final-snapshot"
 
 
   tags = {
     Name = "Aurora MySQL Cluster"
+  }
+}
+
+
+resource "aws_rds_cluster_instance" "aurora_instances" {
+  count             = 2
+  identifier        = "aurora-instance-${count.index}"
+  cluster_identifier = aws_rds_cluster.aurora_cluster.id
+  instance_class    = "db.t3.small"
+  engine            = aws_rds_cluster.aurora_cluster.engine
+  engine_version    = aws_rds_cluster.aurora_cluster.engine_version
+  publicly_accessible = false
+  db_subnet_group_name = aws_db_subnet_group.aurora_subnet_group.name
+
+  tags = {
+    Name = "Aurora Instance ${count.index}"
   }
 }
