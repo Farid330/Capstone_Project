43c1d7ccc273e740ddcae89b6dcf5b06e1d388c5
Farid330 <orujov.farid94@gmail.com>
2024-12-02 16:24:17 +0100

    creating just s3 bucket again to identify the issue

--- S3.tf
+++ S3.tf
@@ -7,28 +7,3 @@ resource "aws_s3_bucket" "wordpress-project" {
     Environment = "Dev"
   }
 }
-
-resource "aws_s3_bucket_versioning" "versioning_example" {
-  bucket = aws_s3_bucket.wordpress-project.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-# Create an IAM Role for EC2 instance
-resource "aws_iam_role" "ec2_s3_role" {
-  name               = "ec2_s3_role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action    = "sts:AssumeRole"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-        Effect    = "Allow"
-        Sid       = ""
-      }
-    ]
-  })
-}
