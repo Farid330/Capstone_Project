b2698631b3bdbc3fb9edfcb62d56c3091b05a0e3
Farid330 <orujov.farid94@gmail.com>
2024-12-03 11:00:28 +0100

    adding IAM role for EC2 instance.will get error on terraform because of lab user restrctions

--- S3.tf
+++ S3.tf
@@ -2,8 +2,29 @@
 resource "aws_s3_bucket" "wordpress-project" {
   bucket = "wordpress-project"
 
+
   tags = {
     Name        = "wordpress s3"
     Environment = "Dev"
   }
+
+}  
+#Gets objectlockconfiguration error due to lab IAM restrictions
+
+###Create an IAM Role for EC2 instance
+resource "aws_iam_role" "ec2_s3_role" {
+  name               = "ec2_s3_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Effect    = "Allow"
+        Sid       = ""
+      }
+    ]
+  })
 }
